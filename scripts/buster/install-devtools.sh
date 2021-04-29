dcce537722654284893d3a74dba58bb747d4fb18
Dennis Neufeld <git@dneufeld.net>
2021-04-29 22:37:01 +0200

    Remove scripts, fix buster-minimal

---
+++ scripts/buster/install-devtools.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -ex
+
+if [["$TAG_SUFFIX" != "minimal" ]]
+then
+    apt-get update && \
+    apt-get install -y build-essential python2-dev python3-dev python2 python3 && \
+    rm -rf /var/lib/apt/lists/*
+fi
